3380432718340d0a6ee0f46dfecd6848b6b63a3a
Travis Johnston <travisjohnston4588@icloud.com>
2020-01-14 13:25:17 -0600

    database.sql updated with table queries

--- database.sql
+++ database.sql
@@ -3,8 +3,43 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
+
+-- Database is named Outdoor_Planner
+
+CREATE TABLE "user"
+(
     "id" SERIAL PRIMARY KEY,
+    "first_name" VARCHAR (15) NOT NULL,
+    "last_name" VARCHAR (15) NOT NULL,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+INSERT INTO "user"
+    ("first_name", "last_name", "username", "password")
+VALUES
+    ("Travis", "Johnston", "TookieJay622", "TravDaSav62");
+
+CREATE TABLE "routes"
+(
+    "id" SERIAL PRIMARY KEY,
+    "route_name" VARCHAR (100) NOT NULL,
+    "user_id" INT
+);
+
+CREATE TABLE "route_points"
+(
+    "id" SERIAL PRIMARY KEY,
+    "longitude" INT,
+    "latitude" INT,
+    "comments_id" INT,
+    "route_id" INT,
+);
+
+CREATE TABLE "comments"
+(
+    "id" SERIAL PRIMARY KEY,
+    "comments" VARCHAR
+);
+
+
--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -2,17 +2,30 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+
+//GET route for past adventures
 router.get('/', (req, res) => {
     
 });
 
-/**
- * POST route template
- */
+
+//POST route for new adventures
 router.post('/', (req, res) => {
+
+});
+
+// PUT route for updating past adventures
+router.put('/:id', (req, res) => {
+
+});
+
+// DELETE route for deleting past adventures
+router.delete('/:id', (req, res) => {
+
+});
+
+// GET route for a specific past adventure
+router.get('/:id', (req, res) => {
 
 });
 
